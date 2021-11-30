bc105cba84db560f9321455adfc602a3f8e73fe2
alexsmik <alexsmik@yandex.ru>
2021-11-30 11:04:13 +0300

    React Validation Form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.css";
+import Form from './components/Form';
+
+const App = () => {
+
+    return (
+        <>
+            <Form/>
+        </>
+    );
+
+}
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,81 @@
+import React, {useState} from "react";
+import Input from "./Input";
+
+const Form = () => {
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const inputs = [
+        {
+        id: 1,
+        name: "username",
+        type: "text",
+        placeholder: "Пользователь",
+        errorMessage:
+        "Имя пользователя должно содержать 3 символа",
+        label: "Пользователь",
+        pattern: "^[A-Za-z0-9]{3}$",
+        required: true,
+    },
+    {
+        id: 2,
+        name: "email",
+        type: "email",
+        placeholder: "Почта",
+        errorMessage: "Неправильный адрес почты",
+        label: "Почта",
+        required: true,
+    },
+    {
+        id: 3,
+        name: "password",
+        type: "password",
+        placeholder: "Пароль",
+        errorMessage:
+        "Пароль должен быть 4 символа",
+        label: "Ваш Пароль",
+        pattern: `^[A-Za-z0-9]{4}$`,
+        required: true,
+    },
+    {
+        id: 4,
+        name: "confirmPassword",
+        type: "password",
+        placeholder: "Повторите пароль",
+        errorMessage: "Пароли не совпадают",
+        label: "Повторите пароль",
+        pattern: values.password,
+        required: true,
+    },
+    ];
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+};
+
+    const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+};
+    return (
+    <div className="app">
+        <form onSubmit={handleSubmit}>
+            <h1>Form Validation</h1>
+            {inputs.map((input) => (
+                <Input
+                    key={input.id}
+                    {...input}
+                    value={values[input.name]}
+                    onChange={onChange}
+                />
+            ))}
+            <button>Отправить</button>
+        </form>
+    </div>
+    );
+};
+
+export default Form;
